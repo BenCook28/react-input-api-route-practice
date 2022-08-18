3ed9aabb5e45ef15ac583594e06a77ba4991e56f
benthomascook28@gmail.com <benthomascook28@gmail.com>
2022-08-18 15:57:18 -0400

    created visual of input form

---
+++ react-input-api-routing-practice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+  <label>
+    Enter an ingredient:
+    <input type="text" name="ingredient" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+    </div>
+  );
+}
+
+export default App;
