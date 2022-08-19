900d496b77afe43ba4e56a2e9c192ff8ee49fe61
benthomascook28@gmail.com <benthomascook28@gmail.com>
2022-08-19 15:09:30 -0400

    updated fetch

--- react-input-api-routing-practice/src/App.js
+++ react-input-api-routing-practice/src/App.js
@@ -1,6 +1,44 @@
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('complexSearch?apiKey=26073aef38ae4bb9a8e0a5f932d4c334&includeIngredients' + ingredient, {
+        method: 'GET',
+        body: JSON.stringify({
+          name: 'John Smith',
+          job: 'manager',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+
   return (
     <div className="App">
       <form>
